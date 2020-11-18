7730637cf420726d6eda2bb4a4429039b84cd883
Momayaz <momayaz.dr.1@gmail.com>
2020-11-18 12:13:23 +0200

    init

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ const methodOverride = require('method-override');
 // Environment variables
 // -------------------------
 require('dotenv').config();
-const HP_API_URL = process.env.HP_API_URL;
+// const HP_API_URL = process.env.HP_API_URL;
 
 // -------------------------
 // Application Setup
 // -------------------------
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
 app.use(express.urlencoded({ extended: true }));
@@ -32,6 +31,7 @@ app.use(express.static('./img'));
 
 // Database Setup
 
+
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
@@ -43,18 +43,38 @@ app.set('view engine', 'ejs');
 // ----------------------
 // ------- Routes -------
 // ----------------------
-
-
+app.get('/home', getHouses);
+// app.get('/detail', showDetails);
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
+function getHouses(req,res){
+    let dataArr = [];
+    let url = 'http://hp-api.herokuapp.com/api/characters';
+    superagent.get(url).then(data=>{
+        data.body.forEach(element => {
+            dataArr.push(new Stories(element));
+            
+        });
+        res.render('home', {result:dataArr});
+    })
+}
 
+// function showDetails(req,res){
+//     let query = 'http://hp-api.herokuapp.com/api/characters';
+//     client.query(query).then(data=>{
+//         res.render
+//     })
+// }
 
 
 // -----------------------------------
 // --- CRUD Pages Routes functions ---
 // -----------------------------------
-
+function Stories(data){
+    this.house = data.house;
+    this.dateOfBirth = data.house.dateOfBirth;
+}
 
 
 // Express Runtime
